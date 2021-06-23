8b54585f77dd8f9bfdadefdfbc879b64b690e34d
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-06-22 21:10:18 -0300

    authentication context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,21 @@ import GoogleIconImg from '../assets/images/google-icon.svg'
 import {Button} from '../components/Button'
 
 import '../styles/auth.scss'
+import { AuthContext } from '../App'
+import { useContext } from 'react'
 
 export function Home(){
     const history = useHistory();
+    const {user, signInWithGoogle} = useContext(AuthContext)
 
-    function navigateToNewRoom(){
+    async function handleCreateRoom(){
+        if(!user){
+            await signInWithGoogle()
+        }
+
         history.push('/rooms/new')
     }
+
     return (
         <div id="page-auth">
             <aside>
@@ -24,7 +32,7 @@ export function Home(){
             <main>
                 <div className="main-content">
                     <img src={Logo} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={GoogleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,17 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../App'
 
 import IllustrationImg from '../assets/images/illustration.svg'
 import Logo from '../assets/images/logo.svg'
-import GoogleIconImg from '../assets/images/google-icon.svg'
 
 import {Button} from '../components/Button'
 
 import '../styles/auth.scss'
 
 export function NewRoom(){
+    const { user } = useContext(AuthContext)
+
     return (
         <div id="page-auth">
             <aside>
@@ -19,6 +22,7 @@ export function NewRoom(){
             <main>
                 <div className="main-content">
                     <img src={Logo} alt="Letmeask" />
+                    <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
                     <form>
                         <input 
