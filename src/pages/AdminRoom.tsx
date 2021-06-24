f23b7abc22b986b14413df9136b38d392703d238
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-06-24 15:43:28 -0300

    create admin page

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,87 @@
+import { useEffect } from 'react'
+import { FormEvent, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { Question } from '../components/Question'
+
+import LogoImg from '../assets/images/logo.svg'
+
+import { Button } from '../components/Button'
+import { RoomCode } from '../components/RoomCode'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+
+import '../styles/Room.scss'
+import { useRoom } from '../hooks/useRoom'
+
+type RoomParams = {
+    id: string,
+}
+
+export function AdminRoom(){
+    const { user } = useAuth()
+    const params = useParams<RoomParams>()
+    const [newQuestion, setNewQuestion] = useState('')
+    const roomId = params.id
+
+    const {title, questions} = useRoom(roomId)
+
+    async function handleSendQuestion(event: FormEvent){
+        event.preventDefault()
+
+        if (newQuestion.trim() === ''){
+            return
+        }
+
+        if(!user){
+            throw new Error('You must be logged in')
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar: user.avatar,
+            },
+            isHighLighted: false,
+            isAnswered: false
+        }
+
+        await database.ref(`rooms/${roomId}/questions`).push(question)
+
+        setNewQuestion('')
+    }
+
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={LogoImg} alt="Letmeask" />
+                    <div>
+                        <RoomCode code={params.id}/>
+                        <Button isOutlined >Encerrar sala</Button>
+                    </div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    { questions.length > 0 && <span> {questions.length} pergunta(s)</span> }
+                </div>
+
+                <div className="question-list">
+                    {questions.map(question =>{
+                        return(
+                            <Question
+                                key={question.id}
+                                content={question.content}
+                                author={question.author}
+                            />
+                        )
+                    })}
+                </div>
+            </main>
+        </div>
+    )
+}
